1600b65bc3e98509450aa170d2521ad445add662
leoleung2000 <leoleung2000@yahoo.com.hk>
2020-06-08 00:49:04 +0800

    Updates

---
+++ precache-manifest.77c6424d0addedc3ddc6e3dddb104b0c.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ccde3fdbfa938b679d849eadb97b9b10",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "d9834dbc1b25d9e07e95",
+    "url": "/portfolio/static/css/main.e0b55855.chunk.css"
+  },
+  {
+    "revision": "4f42ae8fcc4b27b81e55",
+    "url": "/portfolio/static/js/2.e7b953e6.chunk.js"
+  },
+  {
+    "revision": "d9834dbc1b25d9e07e95",
+    "url": "/portfolio/static/js/main.90882d11.chunk.js"
+  },
+  {
+    "revision": "39358b29cbd5cef78848",
+    "url": "/portfolio/static/js/runtime-main.5dabffb3.js"
+  },
+  {
+    "revision": "21f233e19402cc4a66866a7f31191f0d",
+    "url": "/portfolio/static/media/Agustina.21f233e1.woff"
+  },
+  {
+    "revision": "ee6539921d713482b8ccd4d0d23961bb",
+    "url": "/portfolio/static/media/Montserrat-Regular.ee653992.ttf"
+  },
+  {
+    "revision": "e47c3e26372c670c4a4eeac6eea0bf98",
+    "url": "/portfolio/static/media/airbnbLogo.e47c3e26.jpg"
+  },
+  {
+    "revision": "5dbb1903e14983b6af9747de37842b52",
+    "url": "/portfolio/static/media/brain.5dbb1903.png"
+  },
+  {
+    "revision": "f5565ef61e1c96b777908206be05e011",
+    "url": "/portfolio/static/media/computer.f5565ef6.png"
+  },
+  {
+    "revision": "d6d4086fb1cade58b3f786620e17c3ef",
+    "url": "/portfolio/static/media/contactMail.d6d4086f.png"
+  },
+  {
+    "revision": "59389695208d7454c6607bed51ed4316",
+    "url": "/portfolio/static/media/developerActivity.59389695.svg"
+  },
+  {
+    "revision": "52be31441c8cf8a8a16bd7a228d428bf",
+    "url": "/portfolio/static/media/manOnTable.52be3144.svg"
+  },
+  {
+    "revision": "e2c258711bf6a5242dfffba4193bd952",
+    "url": "/portfolio/static/media/web-programming.e2c25871.png"
+  }
+]);
