6710e1e261fd530c867df8bf84918c79eacba3f7
leoleung2000 <leoleung2000@yahoo.com.hk>
2020-06-07 23:57:51 +0800

    Update content

---
+++ src/containers/Main.js
@@ -0,0 +1,31 @@
+import React, { Component } from "react";
+import Header from "../components/header/Header";
+import Greeting from "./greeting/Greeting";
+import Skills from "./skills/Skills";
+// import WorkExperience from "./workExperience/WorkExperience";
+// import StartupProject from "./StartupProjects/StartupProject";
+import Footer from "../components/footer/Footer";
+import Top from "./topbutton/Top";
+import Profile from "./profile/Profile";
+import Service from "./service/Services";
+
+export default class Main extends Component {
+  render() {
+    return (
+      <div>
+        <Header />
+        <Greeting />
+        <Skills />
+        {/* <WorkExperience /> */}
+        <Service />
+        {/* <Projects /> */}
+        {/* <StartupProject /> */}
+        {/* <Achievement /> */}
+        {/* <Twitter /> */}
+        <Profile />
+        <Footer />
+        <Top />
+      </div>
+    );
+  }
+}
